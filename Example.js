41d61f242422004e374d251cc68f2808c1b8d78a
Resma Mathew <35901264+ResmaMathew@users.noreply.github.com>
2019-10-07 15:15:24 +0200

    Create Example.js

---
+++ Example.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+
+const Example= () =>{
+return(
+<Button  color="primary">{t('signin-error')}</Button>
+)
+}
+export default Example
